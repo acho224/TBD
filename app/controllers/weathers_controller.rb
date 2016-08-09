5f4dc5c013fae6af4feb05726ad003c2d6c07b79
Arlen Cho <arlen_cho@hotmail.com>
2016-08-09 00:27:07 -0400

    completed API request/rendering

--- app/controllers/weathers_controller.rb
+++ app/controllers/weathers_controller.rb
@@ -2,14 +2,25 @@ class WeathersController < ApplicationController
     # API_KEY = ENV["WUNDER_KEY"]
 
   def index
-    url = "http://api.wunderground.com/api/799d1ab37e740ec2/conditions/q/90210.json"
-    response = HTTParty.get(url)
-    # puts response
-    @weather_results = response['current_observation']
-    # puts @weather_results
-    puts @weather_results['display_location']['full']
-    puts @weather_results['display_location']['zip']
-    puts @weather_results['temp_f']
-  end
+    # @weather_results = 'poop'
+    key = ENV['WUNDER_KEY']
+    if params[:zipcode]
+      url = "http://api.wunderground.com/api/#{key}/conditions/q/#{params[:zipcode]}.json"
+      url2 = "http://api.wunderground.com/api/#{key}/forecast/q/#{params[:zipcode]}.json"
+      response = HTTParty.get(url)
+      response2 = HTTParty.get(url2)
+      @weather_results = response['current_observation']
+      @precipitation = response2['forecast']['txt_forecast']['forecastday'][0]['pop']
 
+      puts @weather_results['display_location']['full']
+      puts @weather_results['icon_url']
+      puts @weather_results['temp_f']
+      puts @weather_results['weather']
+      puts @precipitation
+
+    respond_to do |format|
+      format.js
+      end
+    end
+  end
 end
